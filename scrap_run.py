6caca6276ce0ae9ec88556b091cf6f4728cda482
Hary Morton-Smith <hmortonsmith@gmail.com>
2019-06-06 15:49:23 +0100

    fixed shauns gibberish

--- scrap_run.py
+++ scrap_run.py
@@ -46,15 +46,14 @@ cursor = IMDB_conn.cursor()
 
 # PART III
 # Add a table to Database (Created DB in SQL
-#cursor.execute("CREATE TABLE IMDB_Movies (VideoType VARCHAR(20), PrimaryTitle VARCHAR(70), OriginalTitle VARCHAR(70), IsAdult BIT, StartYear SMALLINT, EndYear SMALLINT, RunTime SMALLINT, Genres VARCHAR(70));")
+cursor.execute("CREATE TABLE IMDB_Movies (VideoType VARCHAR(20), PrimaryTitle VARCHAR(70), OriginalTitle VARCHAR(70), IsAdult BIT, StartYear SMALLINT, EndYear SMALLINT, RunTime SMALLINT, Genres VARCHAR(70));")
 
 # Add IMDB data to table in SQL DB
 # For each list within my list run an iteration
 # and run a SQL statement on that inner list (or row) adding it to my newly created table
 for entry in IMDB_py_data[1:]:
-    print(entry)
-    #cursor.execute(f"INSERT INTO IMDB_Movies (VideoType, PrimaryTitle, OriginalTitle, IsAdult, StartYear, EndYear, RunTime, Genres) VALUES ('{entry[0]}', '{entry[1]}', '{entry[2]}', '{entry[3]}' , '{entry[4]}' , '{entry[5]}', '{entry[6]}', '{entry[7]}');")
+    cursor.execute(f"INSERT INTO IMDB_Movies (VideoType, PrimaryTitle, OriginalTitle, IsAdult, StartYear, EndYear, RunTime, Genres) VALUES ('{entry[0]}', '{entry[1]}', '{entry[2]}', '{entry[3]}' , '{entry[4]}' , '{entry[5]}', '{entry[6]}', '{entry[7]}');")
 
 
 # Commit my changes to the SQL database
-#IMDB_conn.commit()
+IMDB_conn.commit()
